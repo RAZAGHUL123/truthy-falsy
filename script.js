3504b6c55e771a6a8a57318a89e3058a1b69bce1
Rafael Moreno <92047284+RAZAGHUL123@users.noreply.github.com>
2023-04-20 18:36:31 -0700

    a change

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function checkValue() {
 	const value = document.getElementById("input").value;
 	const result = document.getElementById("result");
 	
-	result.innerHTML = value ? `${value} is truthy!` : `${value} is falsy!`;
-	result.style.color = value == false ? "red" : "black";
+	result.innerHTML = value || value === 0 ? `${value} is truthy!` : `${value} is falsy!`;
+	result.style.color = value || value === 0 ? "black" : "red";
   }
+
   
